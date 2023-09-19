2b8d3d073ce36d439f356e5366ebc57fbbf81304
Feng Li <lifeng@microsoft.com>
2023-09-20 00:33:46 +0800

    stack

--- 1.两数之和.py
+++ 1.两数之和.py
@@ -5,12 +5,12 @@
 #
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dic={}
-        res=[]
+        dic = {}
+        res = []
         for i in range(len(nums)):
-            if dic.get(target-nums[i])!=None:
-                res.append(dic[target-nums[i]])
+            if dic.get(target - nums[i]) != None:
+                res.append(dic[target - nums[i]])
                 res.append(i)
                 break
-            dic[nums[i]]=i
+            dic[nums[i]] = i
         return res
--- 84. 柱状图中最大的矩形.py
+++ 84. 柱状图中最大的矩形.py
@@ -1,7 +1,9 @@
-# 单调栈
+# 单调栈(递增)
 # 对于每个位置的heights[i]，矩形面积为到两边小于其高度为止的宽度
 # 使用单调栈，如果栈顶height较大就弹出
 from typing import List
+
+
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         size = len(heights)
@@ -15,3 +17,6 @@ class Solution:
                 area = max(area, heights[stack.pop()] * (i - stack[-1] - 1))
             stack.append(i)
         return area
+
+
+print(Solution().largestRectangleArea([3, 4, 5, 6]))
